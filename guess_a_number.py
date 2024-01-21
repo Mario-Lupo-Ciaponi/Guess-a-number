1b86578219c7897320aad9ca0b79d0fa68c4cba7
mariociaponi <mario.lupo.ciaponi08@gmail.com>
2024-01-21 13:12:57 +0200

    Updated the file and now it could be played.

--- guess_a_number.py
+++ guess_a_number.py
@@ -1,3 +1,19 @@
 import random
 
-number = int(input())
+print("Hello! In this game you have to guess the number from 1-10. When you guess it we will print you the number of"
+      "tries.\nNote: This is a beta and new features will be added later on.")
+
+random_number = random.randint(0, 10)
+
+number = int(input("Guess the number: "))
+tries = 1
+while random_number != number:
+    if number > random_number:
+        print("Too high!")
+    else:
+        print("Too low!")
+
+    number = int(input("Guess the number: "))
+    tries += 1
+
+print(f"Well done! The number is {random_number}\nTries: {tries}")
