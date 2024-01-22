4a08166b2bb79179c241b7efc5087e8f67bae840
mariociaponi <mario.lupo.ciaponi08@gmail.com>
2024-01-22 17:20:01 +0200

    Added a gamemode and changed readme file.

--- guess_a_number.py
+++ guess_a_number.py
@@ -6,6 +6,26 @@ print("Hello! In this game you have to guess the number from 1-10. When you gues
 print()
 print("=============================================================================================================")
 
+print("Do you want your tries to be limited? (Yes/No)")
+
+
+cap_of_tries = 0
+limited_tries_game_mode = False
+while True:
+    answer = input()
+
+    if answer == "Yes":
+        print("Limited tries gamemode activated.")
+        limited_tries_game_mode = True
+    elif answer == "No":
+        print("Normal gamemode activated.")
+    else:
+        print("Invalid answer. Please type again.")
+        continue
+
+    break
+print()
+
 print("Please chose a difficulty:")
 print("1. Easy(random number in range from 1-10) \n2. Normal(random number in range from 1-100)\n"
       "3. Hard(random number from 1-1000) \n4. Very hard(random number from 1-10000)\n"
@@ -21,21 +41,39 @@ while True:
     if difficulty == 1:
         random_number = random.randint(0, 10)
         difficulty_in_string = "Easy"
+
+        if limited_tries_game_mode:
+            cap_of_tries = 5
     elif difficulty == 2:
         random_number = random.randint(1, 100)
         difficulty_in_string = "Normal"
+
+        if limited_tries_game_mode:
+            cap_of_tries = 15
     elif difficulty == 3:
         random_number = random.randint(1, 1000)
         difficulty_in_string = "Hard"
+
+        if limited_tries_game_mode:
+            cap_of_tries = 20
     elif difficulty == 4:
         random_number = random.randint(1, 10000)
         difficulty_in_string = "Very hard"
+
+        if limited_tries_game_mode:
+            cap_of_tries = 30
     elif difficulty == 5:
         random_number = random.randint(1, 100000)
         difficulty_in_string = "Extreme"
+
+        if limited_tries_game_mode:
+            cap_of_tries = 40
     elif difficulty == 6:
         random_number = random.randint(1, 1000000)
         difficulty_in_string = "Impossible"
+
+        if limited_tries_game_mode:
+            cap_of_tries = 50
     else:
         print("Invalid difficulty. Please type again:")
         continue
@@ -46,8 +84,13 @@ print("=========================================================================
 print()
 
 print(f'You chose "{difficulty_in_string}"')
+if limited_tries_game_mode:
+    print(f"Cap of tries: {cap_of_tries}")
+
 number = int(input("Guess the number: "))
 tries = 1
+
+failed = False
 
 while random_number != number:
     if number > random_number:
@@ -55,9 +98,27 @@ while random_number != number:
     else:
         print("Too low!")
 
+    if limited_tries_game_mode:
+        if cap_of_tries < tries:
+            failed = True
+            break
+
     number = int(input("Guess the number: "))
     tries += 1
 
 print("=============================================================================================================")
 print()
-print(f"Well done! The number is {random_number}!\nTries: {tries}")
+
+if not failed:
+    print(f"Well done! The number is {random_number}!\nTries: {tries}")
+else:
+    print(print("Out of tries! Better luck next time :)"))
+print()
+
+print("Developers high score:\n"
+      "Easy: 1 try\n"
+      "Normal: 5 tries\n"
+      "Hard: 22 tries\n"
+      "Very hard: 32 tries\n"
+      "Extreme: (not yet tried)\n"
+      "Impossible: (not yet tried)\n")
